0c052e76b9356b81d1c8e822e7289d1d0686991c
robertej19 <robertej@mit.edu>
2020-07-17 19:41:48 -0400

    test

--- src/SubMit.py
+++ src/SubMit.py
@@ -186,9 +186,9 @@ def setup_database(args):
         use_mysql = True
         if args.test_database:
             cred_file_name = '/../..'+fs.test_db_cred_file #the ../ is needed due to the path difference in client/src and utils/
-            database_name = '/../..'+fs.MySQL_Test_DB_Name
+            database_name = fs.MySQL_Test_DB_Name
         else:
-            cred_file_name = fs.prod_db_cred_file
+            cred_file_name = '/../..'+fs.prod_db_cred_file
             database_name = fs.MySQL_Prod_DB_Name
             
         cred_file_loc = os.path.dirname(os.path.abspath(__file__)) + cred_file_name
